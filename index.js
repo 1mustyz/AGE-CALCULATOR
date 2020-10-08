b4a822ac2ea28dede194296569b840fb9cee0f10
1mustyz <onemusty.z@gmail.com>
2020-10-08 14:49:56 +0100

    modified for mr ola birthday

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ const ageOutput = document.querySelector('.age-output');
 const hobbyOutput = document.querySelector('.hobby-output');
 const msg = document.querySelector('.msg');
 
+const birthday = document.querySelector('.birthday');
+const frame1 = document.querySelector('.sub-inner');
+
 
 
 const currentYear = 2020;
-
+let isBlue = false;
+setInterval(function(){
+    if (isBlue){
+        mainButton.style.background = 'pink';
+    }else {
+        mainButton.style.background = '#4349D3';
+    }
+    isBlue = !isBlue;
+},1000);
 
 function mainButtonCallback(){
     modalOuter.classList.add('open');
@@ -47,11 +58,51 @@ function formSubmitCallBack(e){
         alert('please type a valid input');
         return;
     }else{
-        hobbyOutput.textContent += hbb;
-        nameOutput.textContent += name;
-        ageOutput.textContent += currentYear - db;
-        msg.textContent += (currentYear - db) * 365;
+
+        setTimeout(() => {
+            nameOutput.innerHTML = `<span id="b-bold">Mr. ${name} <span>`;
+            setTimeout(() => {
+                ageOutput.innerHTML = `<span id="b-bold">You are ${currentYear - db} old today, and its your birthday <span>`;
+                setTimeout(() => {
+                    msg.innerHTML = `<span id="b-bold">You have lived for ${(currentYear - db) * 365} days on earth <span>`;
+                    setTimeout(() => {
+                        hobbyOutput.innerHTML = `<span id="b-bold">You love ${hbb} as your hobby <span>`; ;
+
+                    }, 4000);
+                },3000);
+            }, 2000);
+        }, 1000);
     }
+    //let i = 0;
+    //while(i < 10){
+        setInterval(() => {
+            setTimeout(()=>{
+                birthday.classList.add('font1');
+        
+                setTimeout(()=>{
+                   // birthday.classList.remove('font1');
+                    birthday.classList.add('font2');
+        
+                    setTimeout(()=>{
+                        //birthday.classList.remove('font2');
+                        birthday.classList.add('font3');
+            
+                        
+                    },5000);
+                },1000);
+            },2000);
+           
+        }, 3000);
+        
+        setInterval(() => {
+            frame1.classList.add('red')
+            setTimeout(() => {
+                frame1.classList.remove('red');
+            }, 500);
+        }, 1000);
+        
+    
+    
 
     modalInner.classList.add('hidden');
     subModalInner.classList.remove('hidden');
@@ -72,6 +123,8 @@ function formResetCallBack(e){
     dob.value = '';
     
 }
+
+
 
 mainButton.addEventListener('click', mainButtonCallback);
 formSubmit.addEventListener('click',formSubmitCallBack);
